76110b0717b79257ac24917ce9962a3645fc055b
Sanna <santras@users.noreply.github.com>
2020-09-25 15:21:01 +0300

    Viikko 3- Lukuvälit, toimii testisyötteillä

--- intervals.py
+++ intervals.py
@@ -7,8 +7,22 @@
 
     
 
-def count(t):
+def count(tt):
     # TODO
+    tt.sort()
+    #print(tt)
+    valit = []
+    valin_alku = 0  # Vuorossa olevan välin alkupä
+
+    for ii in range(len(tt)-1):
+        if (tt[ii+1]-tt[ii]) > 1 :        # jos seuraava alkio ei ole seuraava luku järjestyksessä
+            valit.append([valin_alku,ii])
+            valin_alku = ii+1
+
+    # lisätään viimeinen väli
+    valit.append([valin_alku,len(tt)])
+    return (len(valit))        
+
 
 if __name__ == "__main__":
     print(count([4,1,5,3])) # 2
