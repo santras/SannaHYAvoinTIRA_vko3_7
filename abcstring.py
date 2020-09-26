e603ed36a9a31dd6b66e9bf2a1907999e35ac127
Sanna <santras@users.noreply.github.com>
2020-09-26 09:37:16 +0300

    Viikko 3- ABC-merkkijono, toimii

--- abcstring.py
+++ abcstring.py
@@ -6,13 +6,11 @@
 def solve(kirjainjono):
     pituus = len(kirjainjono)
     jarjestetty = sorted(kirjainjono)
-    print(kirjainjono)
-    print(jarjestetty)
 
     # Kuinka paljon alusta yksittäisiä kirjaimia
     A_pituus = 0 
     B_pituus = 0
-    C_pituus = 0
+
     for ii in range(pituus):
         if jarjestetty[ii] == 'A':
             A_pituus += 1
@@ -20,44 +18,50 @@ def solve(kirjainjono):
             B_pituus += 1
         else:
             break
-    C_pituus = pituus - A_pituus - B_pituus
 
-    # Lasketaan siirrot:
-    # A_vaarin = []
-    # B_vaarin = []
-    # C_vaarin = []
-    siirtoja = 0
+   # Katsotaan erikseen A,B:n paikat ja pistetään ideksit muistiin missä kirjaimia väärällä paikalla
+   # C:tä ei tarvitse erikseen katsoa, koska vaihdetaan aina päikseen niin C korjaantuu kun A ja B korjattu
+    A_vaarin = []   # Lista A kirjaimen idekseistä jotka ovat A-alueen ulkopuolella
+    B_vaarin = []   # Lista B kirjaimen idekseistä jotka ovat A-alueen ulkopuolella
+
     for ii in range(pituus):
         if ii < A_pituus :                                          # A:n paikat
             if kirjainjono[ii] == 'B':
                 B_vaarin.append(ii)
-            elif kirjainjono [ii] == 'C':
-                C_vaarin.append(ii)
         if (ii >= A_pituus) and ii < (A_pituus + B_pituus) :        # B:n paikat
             if kirjainjono[ii] == 'A':
                 A_vaarin.append(ii)
-            elif kirjainjono[ii] == 'C':
-                C_vaarin.append(ii)
-        else:                                                       # C:n paikat
+        elif ii >= (A_pituus + B_pituus):                           # C:n paikat
             if kirjainjono[ii] == 'A':
                 A_vaarin.append(ii)
             elif kirjainjono[ii] == 'B':
                 B_vaarin.append(ii)
-    print (A_vaarin,B_vaarin,C_vaarin)
+   
+    #Tarvittava siirtojen määrä kun vaihdetaan pois väärät kirjaimet A:n paikoilta
+    siirtoja = 0
+    A_ind_alkupaa = 0   # Indeksi virheellisesti oleville A-kirjaimille alkupäästä
+    B_fiksattuja = 0
+    for ii in range(A_pituus):
+        if kirjainjono[ii] != 'A':                
+            siirtoja += 1
+            # kokeillaan saadaanko fiksattua samalla B:n  vääriä paikkoja
+            if (kirjainjono[ii] == 'B') and ( A_vaarin[A_ind_alkupaa] < (A_pituus + B_pituus) ):             # C:t vaihdettaisiin mielummin loppupään kirjaimien kanssa
+                B_fiksattuja += 1
+                A_ind_alkupaa += 1
+            elif (kirjainjono[ii] == 'B'):
+                A_ind_alkupaa += 1
+    
+    # Lisätään siirtoihin virheellisesti sijoitetut B:t lukuunottamatta jo A:n siiroilla fiksattuja
+    siirtoja += len(B_vaarin) - B_fiksattuja                # C:t korjaantuvat samalla
+    return siirtoja
 
-    # Siirtojen määrä
-    # siirtoja = 0
-    # for ii in range(pituus):
-    #     if ii < A_pituus and kirjainjono[ii] != 'A':                # A:n paikat
-    #         siirtoja += 1
-    #         # kokeillaan saadaanko fiksattua samalla B:n ja C:n vääriä paikkoja
-    #         if (kirjainjono[ii] == 'B') and (A_vaarin[0] < (A_pituus + B_pituus) ): 
-                
+
 
 
 
 
 if __name__ == "__main__":
     print(solve("CCAABB")) # 4
-    #print(solve("CBACBA")) # 3
-    #print(solve("AAAA")) # 0
+    print(solve("CBACBA")) # 3
+    print(solve("AAAA")) # 0
+    print(solve("ABBBAABCA")) # 4
